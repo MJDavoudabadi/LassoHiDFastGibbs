310c4acd4f836f49f1e835157adea7df5a098493
Mohammad Javad Davoudabadi <mjavad.davodabadi@gmail.com>
2026-01-18 09:44:23 +1000

    Changed the name of the package

--- R/LassoHiDFastGibbs-package.R
+++ R/LassoHiDFastGibbs-package.R
@@ -1,13 +1,13 @@
-#' @title Fast Gibbs Samplers: Blocked, Partially Collapsed, and Nested Gibbs Samplers
+#' @title Fast High-Dimensional Gibbs Samplers for Bayesian Lasso Regression
 #'
 #' @description Provides fast and scalable Gibbs sampling algorithms for
-#' Bayesian penalized linear regression models in high-dimensional
+#' Bayesian Lasso regression model in high-dimensional
 #' settings. The package implements efficient partially collapsed
 #' and nested Gibbs samplers for Bayesian Lasso, with a focus on
 #' computational efficiency when the number of predictors is large
 #' relative to the sample size.
-#' @name FastGibbsSamplers
+#' @name LassoHiDFastGibbs
 #' @docType package
-#' @useDynLib FastGibbsSamplers, .registration = TRUE
+#' @useDynLib LassoHiDFastGibbs, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 "_PACKAGE"
