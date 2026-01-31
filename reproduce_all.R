31645b2e5dec52e201a7367ebb7737482e3ae93a
Mohammad Javad Davoudabadi <mohammad.davoudabadi@sydney.edu.au>
2026-01-31 10:50:30 +1000

    modified a file

--- reproduce_all.R
+++ reproduce_all.R
@@ -2,30 +2,6 @@ pkg <- "LassoHiDFastGibbs"
 
 
 
-dir.create("reproduction/helpers", recursive = TRUE, showWarnings = FALSE)
-
-file.copy(
-  from = c("inst/benchmarks/helpers/benchmark_3bg.R",
-           "inst/benchmarks/helpers/benchmark_4bg.R",
-           "inst/benchmarks/helpers/benchmark_blasso_bayeslm.R",
-           "inst/benchmarks/helpers/benchmark_blasso_bayesreg.R",
-           "inst/benchmarks/helpers/benchmark_blasso_hans.R",
-           "inst/benchmarks/helpers/benchmark_blasso_monomvn.R",
-           "inst/benchmarks/helpers/benchmark_blasso_rstan.R",
-           "inst/benchmarks/helpers/benchmark_EHS.R",
-           "inst/benchmarks/helpers/benchmark_horseshoe.R",
-           "inst/benchmarks/helpers/benchmark_horseshoe_bayeslm.R",
-           "inst/benchmarks/helpers/benchmark_horseshoe_bayesreg.R",
-           "inst/benchmarks/helpers/benchmark_horseshoe_brms.R",
-           "inst/benchmarks/helpers/generate_data.R",
-           "inst/benchmarks/helpers/plot_densities.R",
-           # these two are in R/
-           "R/effective_sample_size.R",
-           "R/mcmc_stats.R"),
-  to = "reproduction/helpers",
-  overwrite = TRUE
-)
-
 
 
 if (!requireNamespace(pkg, quietly = TRUE)) {
